d3953294502c47a3ac67a9fb9c710d9396973807
stephane boguhe <boguhestephane97@gmail.com>
2021-06-27 13:29:11 +0100

    changement de titre

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,8 +22,8 @@ const Home = () => {
                         </p>
                     </div>
                     <div className="second">
-                        <h3 className="text-center">Stephane En Bref</h3>
-                        <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise 
+                        <h3 className="text-center">mon vecu</h3>
+                        <p>Le Lorem Ipsum du faux texte employé dans la composition et la mise 
                         en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie 
                         depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte 
                         pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre 
